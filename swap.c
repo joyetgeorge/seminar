5e1a234122eef41617b2916810c56be1ff3e5510
joyet <joyetgeorge07@gmail.com>
2021-07-25 11:25:21 +0530

    branch merged

--- swap.c
+++ swap.c
@@ -2,13 +2,17 @@ void main()
 {
     int a = 2;
     int b = 8;
-
-    //swapping......
     int temp;
 
-    temp = a;
-    a = b;
-    b = temp;
+    void swap(int *a, int *b)
+    {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    
+
+    swap(&a, &b)
 
 
     print("a = %d", &a);
