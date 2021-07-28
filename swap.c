b0137414b6c9ec79f218db4b03413e27c88177ae
joyet <joyetgeorge07@gmail.com>
2021-07-28 20:10:30 +0530

    added swap logic

--- swap.c
+++ swap.c
@@ -3,10 +3,12 @@ void main()
     int a = 2;
     int b = 8;
 
-    //swapping....
+
     int temp = a;
     a = b;
     b = temp;
+    //swapping....
+    
     
     print("a = %d", &a);
     print("b = %d", &b);
