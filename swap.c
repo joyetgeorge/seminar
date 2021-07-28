575a9ed37737bb5674034dd145576849436ee833
Joyet George <joyetgeorge07@gmail.com>
2021-07-28 22:11:14 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -3,14 +3,6 @@ void main()
     int a = 2;
     int b = 8;
 
-
-    int temp = a;
-    a = b;
-    b = temp;
     //swapping....
     
-    
-    print("a = %d", &a);
-    print("b = %d", &b);
-
 }
